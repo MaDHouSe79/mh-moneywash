6874cbb88599a1c732a7aded8a7b700a2507e663
MaDHouSe <madhouse1979@gmail.com>
2024-02-03 01:07:19 +0100

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -1,5 +1,5 @@
 --[[ ===================================================== ]] --
---[[          MH Blackmoney Wash Script by MaDHouSe        ]] --
+--[[            MH Money Wash Script by MaDHouSe           ]] --
 --[[ ===================================================== ]] --
 local QBCore = exports['qb-core']:GetCoreObject()
 
