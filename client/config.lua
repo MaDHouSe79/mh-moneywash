34104d40a653a56aa5d3e95dc49b65c28f6a9d86
MaDHouSe <madhouse1979@gmail.com>
2024-02-20 22:00:05 +0100

    Add files via upload

---
+++ client/config.lua
@@ -0,0 +1,13 @@
+--[[ ===================================================== ]] --
+--[[          MH Blackmoney Wash Script by MaDHouSe        ]] --
+--[[ ===================================================== ]] --
+Config = {}
+
+Config.Inventory = "ox_inventory" -- qb-inventory or ox_inventory
+Config.Target = "ox_target"       -- qb-target or ox_target
+Config.Blip = {
+    label = 'Washmachine',        -- blip label (stacked)
+    sprite = 434,                 -- blip icon (https://docs.fivem.net/docs/game-references/blips/)
+    color = 32,                   -- blip color
+    enable = false                -- if true it shows blips on the map
+}
