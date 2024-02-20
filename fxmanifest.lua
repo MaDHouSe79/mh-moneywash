fa4f3d411108f310ff66861ac74b3cf4066fbb80
MaDHouSe <madhouse1979@gmail.com>
2024-02-20 22:01:04 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,12 @@
 --[[ ===================================================== ]] --
---[[            MH Money Wash Script by MaDHouSe           ]] --
+--[[          MH Blackmoney Wash Script by MaDHouSe        ]] --
 --[[ ===================================================== ]] --
 fx_version 'cerulean'
 game 'gta5'
-description 'MH Money Wash'
+description 'MH Blackmoney Wash'
 version '1.0'
-shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua', 'config.lua'}
-client_scripts {'client/main.lua'}
-server_scripts {'server/main.lua', 'server/update.lua'}
+shared_scripts {'@qb-core/shared/locale.lua', 'locales/en.lua'}
+client_scripts {'client/config.lua', 'client/main.lua'}
+server_scripts {'@oxmysql/lib/MySQL.lua', 'server/sv_config.lua', 'server/main.lua', 'server/update.lua'}
+dependencies {'qb-core'}
+lua54 'yes'
